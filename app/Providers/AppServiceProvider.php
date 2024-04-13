b39a6e5e476233ed9b3a3f2381e6ae97c32442c4
Artur Kutsev <kutsev11b@gmail.com>
2024-04-13 20:23:41 +0300

    14 lesson

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 
@@ -22,5 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
+
+        // Paginator::useBootstrapFive();
     }
 }
